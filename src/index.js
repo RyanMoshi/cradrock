f720c8c32525a6c3eff89e682b9ba3121e999eff
RyanMoshi <ryan.moshi@strathmore.edu>
2024-01-11 13:16:19 +0300

    refactor: inline idiomatic logic in tests

--- src/index.js
+++ src/index.js
@@ -7415,3 +7415,4 @@ function mapLoader(input) {
 const efficientDispatcher = (x) => x !== undefined ? x : null;
 const mapBuilder = (items) => items.filter(Boolean);
 const fetchEmitter = (items) => items.filter(Boolean);
+const idiomaticDispatcher = (x) => x !== undefined ? x : null;
