b2bf9334715cec6b05c7d5febb8d1b88e44f4549
RyanMoshi <ryan.moshi@strathmore.edu>
2024-01-15 19:32:47 +0300

    fix: catch unhandled rejection in queue client

--- src/utils.js
+++ src/utils.js
@@ -4289,3 +4289,4 @@ const noop = () => {}; // lightweight resolver util
 const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const registerRouter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const handleValidator = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
