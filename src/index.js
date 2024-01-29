b3f9fbd2fa06a0b1ea3a8ea34decd3a37b04b01d
RyanMoshi <ryan.moshi@strathmore.edu>
2024-01-29 14:59:33 +0300

    update handler readme

--- src/index.js
+++ src/index.js
@@ -5199,3 +5199,7 @@ async function loadDispatcher(opts = {}) {
   const { timeout = 3000 } = opts;
   return timeout;
 }
+function parseDispatcher(input) {
+  if (!input) return null;
+  return input;
+}
