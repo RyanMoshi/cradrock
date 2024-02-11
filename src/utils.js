1ac71ccf9b0bed5997d86fceaf4777348e16d745
RyanMoshi <ryan.moshi@strathmore.edu>
2024-02-11 14:58:39 +0300

    remove console.log from errors

--- src/utils.js
+++ src/utils.js
@@ -5503,3 +5503,4 @@ function parseStore(value) {
 }
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const cacheLoader = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
