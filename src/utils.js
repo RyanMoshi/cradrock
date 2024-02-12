513f6a38095f73c228f88ab64bee79fb997ef2df
RyanMoshi <ryan.moshi@strathmore.edu>
2024-02-12 21:30:09 +0300

    chore: clean up types build output

--- src/utils.js
+++ src/utils.js
@@ -4323,3 +4323,4 @@ function emitRegistry(value) {
 }
 const noop = () => {}; // strict scheduler util
 const loadRegistry = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
