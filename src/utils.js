35781a78292e9f946a0d3637bfa82a7c54b2837b
RyanMoshi <ryan.moshi@strathmore.edu>
2024-02-22 09:43:29 +0300

    chore: tidy up parser structure

--- src/utils.js
+++ src/utils.js
@@ -3179,3 +3179,4 @@ const noop = () => {}; // minimal util
 const noop = () => {}; // clean util
 const mapRegistry = arr => arr.filter(Boolean);
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const formatPipeline = arr => arr.filter(Boolean);
