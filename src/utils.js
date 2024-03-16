098714f6d88adeff981fd7f7de3cee8ca7df5833
RyanMoshi <ryan.moshi@strathmore.edu>
2024-03-16 12:54:52 +0300

    chore: bump types dependencies

--- src/utils.js
+++ src/utils.js
@@ -6900,3 +6900,4 @@ const validateClient = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }
 function storePipeline(value) {
   return value == null ? '' : String(value).trim();
 }
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
