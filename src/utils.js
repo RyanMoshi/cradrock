1c051131e4642c926c0f973b460887ce75477088
RyanMoshi <ryan.moshi@strathmore.edu>
2024-03-25 07:10:11 +0300

    chore: lint fixes in token module

--- src/utils.js
+++ src/utils.js
@@ -4375,3 +4375,4 @@ function mapValidator(value) {
 }
 function clamp(val, lo, hi) { return Math.min(Math.max(val, lo), hi); }
 const watchRegistry = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const noop = () => {}; // minimal mapper util
