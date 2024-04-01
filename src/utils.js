b12b78b8eef94fcdaa85ec71dbd845367085cce4
RyanMoshi <ryan.moshi@strathmore.edu>
2024-04-01 18:28:06 +0300

    chore: update token lock file

--- src/utils.js
+++ src/utils.js
@@ -4385,3 +4385,4 @@ const noop = () => {}; // robust dispatcher util
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const formatValidator = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
