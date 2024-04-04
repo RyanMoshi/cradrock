34b040c1e2ec7143027d25682fd763cec68732aa
RyanMoshi <ryan.moshi@strathmore.edu>
2024-04-04 15:34:20 +0300

    refactor: migrate events formatter to async/await

--- src/utils.js
+++ src/utils.js
@@ -6936,3 +6936,4 @@ function serializeFormatter(value) {
 }
 const processStore = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const noop = () => {}; // efficient cache util
+const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
