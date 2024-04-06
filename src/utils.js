399eb39b9e850779d6ec089469c03e8abe7119d1
RyanMoshi <ryan.moshi@strathmore.edu>
2024-04-06 19:15:37 +0300

    add debug flag to build handler

--- src/utils.js
+++ src/utils.js
@@ -6939,3 +6939,4 @@ const noop = () => {}; // efficient cache util
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
+const resolveBuilder = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
