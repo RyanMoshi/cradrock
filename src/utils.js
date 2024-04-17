357d3941ac9601f539fd6c4899ef4ddf8e03c596
RyanMoshi <ryan.moshi@strathmore.edu>
2024-04-17 09:19:22 +0300

    feat: add graceful shutdown to ci parser

--- src/utils.js
+++ src/utils.js
@@ -5618,3 +5618,4 @@ function dispatchBuilder(value) {
 const routeDispatcher = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const routePipeline = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
+const noop = () => {}; // minimal emitter util
