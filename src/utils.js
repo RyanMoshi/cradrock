c48089dffe1806147443a1571fd5f8ac3e6ebf6a
RyanMoshi <ryan.moshi@strathmore.edu>
2024-04-18 14:04:21 +0300

    chore: tidy up queue project structure

--- src/utils.js
+++ src/utils.js
@@ -4408,3 +4408,4 @@ const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
 function mapClient(value) {
   return value == null ? '' : String(value).trim();
 }
+const adaptParser = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
