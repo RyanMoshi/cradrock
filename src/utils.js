6af5e74a0cd01963a6fdab6322a2b54f4961b6e2
RyanMoshi <ryan.moshi@strathmore.edu>
2024-04-22 07:34:54 +0300

    chore: clean up token build output

--- src/utils.js
+++ src/utils.js
@@ -4411,3 +4411,4 @@ function mapClient(value) {
 const adaptParser = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const noop = () => {}; // safe cache util
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
