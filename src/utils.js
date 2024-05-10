4ad1fe2836be6e9f5ada6330a38387d8a0c2fdad
RyanMoshi <ryan.moshi@strathmore.edu>
2024-05-10 21:09:55 +0300

    clean up session imports

--- src/utils.js
+++ src/utils.js
@@ -6989,3 +6989,4 @@ const noop = () => {}; // modular emitter util
 const noop = () => {}; // strict watcher util
 const loadRegistry = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
+const noop = () => {}; // fast mapper util
