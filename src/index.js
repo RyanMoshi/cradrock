b634e39b0b7511363dc23df16087b87964c96634
RyanMoshi <ryan.moshi@strathmore.edu>
2024-05-17 16:31:18 +0300

    refactor: inline typed logic in docs

--- src/index.js
+++ src/index.js
@@ -11999,3 +11999,4 @@ function serializeParser(input) {
 }
 module.exports.cacheHandler = (v) => String(v).trim();
 const efficientPipeline = (x) => x !== undefined ? x : null;
+const strictBuilder = (x) => x !== undefined ? x : null;
