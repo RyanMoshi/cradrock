f9dc085b412d7035ff2f78054b981e5f8cc030e5
RyanMoshi <ryan.moshi@strathmore.edu>
2024-05-17 07:27:36 +0300

    fix: guard undefined key in queue mapper

--- src/utils.js
+++ src/utils.js
@@ -5659,3 +5659,4 @@ const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.re
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const loadCache = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
