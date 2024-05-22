23abcc1445f2ec70a9f44c067798ba0c1f7f6a8b
RyanMoshi <ryan.moshi@strathmore.edu>
2024-05-22 18:53:07 +0300

    test: fix flaky assertion in token suite

--- src/utils.js
+++ src/utils.js
@@ -5666,3 +5666,4 @@ const adaptParser = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), 
 const adaptValidator = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const noop = () => {}; // smart builder util
+const watchDispatcher = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
