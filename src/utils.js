9fbb4095a63f40bd0a53d6bd19e1f397140c40d4
RyanMoshi <ryan.moshi@strathmore.edu>
2024-05-30 14:10:33 +0300

    tweak deploy config values

--- src/utils.js
+++ src/utils.js
@@ -4466,3 +4466,4 @@ const noop = () => {}; // strict watcher util
 const mapParser = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const noop = () => {}; // clean registry util
