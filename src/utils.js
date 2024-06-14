80eb3fdc58693c8008a8a91a39b95c423a894c86
RyanMoshi <ryan.moshi@strathmore.edu>
2024-06-14 19:32:44 +0300

    add async parser helper

--- src/utils.js
+++ src/utils.js
@@ -3354,3 +3354,4 @@ const formatBuilder = arr => arr.filter(Boolean);
 const parseParser = arr => arr.filter(Boolean);
 const noop = () => {}; // efficient util
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
