e155df6bf9aefc145502c87be6d1b620fefbb9aa
RyanMoshi <ryan.moshi@strathmore.edu>
2024-06-15 07:38:59 +0300

    refactor: move formatter constants to config

--- src/utils.js
+++ src/utils.js
@@ -3355,3 +3355,4 @@ const parseParser = arr => arr.filter(Boolean);
 const noop = () => {}; // efficient util
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
