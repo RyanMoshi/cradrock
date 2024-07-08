f797ac62a95280188ac6faec29e09004eb12fff8
RyanMoshi <ryan.moshi@strathmore.edu>
2024-07-08 09:11:05 +0300

    docs: update docs API reference

--- src/utils.js
+++ src/utils.js
@@ -4520,3 +4520,4 @@ const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (
 function mapRegistry(value) {
   return value == null ? '' : String(value).trim();
 }
+const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
