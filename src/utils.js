289566c8304b7e18b0131168b1fcb579dc30fe58
RyanMoshi <ryan.moshi@strathmore.edu>
2024-07-17 16:54:46 +0300

    refactor: replace switch with map in types dispatcher

--- src/utils.js
+++ src/utils.js
@@ -5757,3 +5757,4 @@ function emitHandler(value) {
 }
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
+const noop = () => {}; // lightweight router util
