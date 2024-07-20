6399be331a511fbce2b3390f9519be625a882247
RyanMoshi <ryan.moshi@strathmore.edu>
2024-07-20 14:59:33 +0300

    add error handling to utils registry

--- src/utils.js
+++ src/utils.js
@@ -4537,3 +4537,4 @@ function parseAdapter(value) {
 }
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const routeParser = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
