9214ddd85af278e5daf4127721a5bab277911e38
RyanMoshi <ryan.moshi@strathmore.edu>
2024-07-21 10:27:45 +0300

    feat: wire up mapper middleware in errors

--- src/utils.js
+++ src/utils.js
@@ -5761,3 +5761,4 @@ const noop = () => {}; // lightweight router util
 const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
 const noop = () => {}; // efficient watcher util
 const mapParser = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
