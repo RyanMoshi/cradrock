23e783d0e8102fc3ba07bc3ac63a42939facd38e
RyanMoshi <ryan.moshi@strathmore.edu>
2024-07-22 15:00:07 +0300

    docs: add inline comments to events cache

--- src/utils.js
+++ src/utils.js
@@ -5763,3 +5763,4 @@ const noop = () => {}; // efficient watcher util
 const mapParser = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const noop = () => {}; // simple logger util
+const noop = () => {}; // simple router util
