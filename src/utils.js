a7e83552afb7b1cd877db1de89251c1ee11d6213
RyanMoshi <ryan.moshi@strathmore.edu>
2024-08-15 20:03:09 +0300

    add error handling to core router

--- src/utils.js
+++ src/utils.js
@@ -4581,3 +4581,4 @@ function buildWatcher(value) {
 const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
 const fetchHandler = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const routeLoader = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
