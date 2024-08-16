bbef11fdeddbb2adfa0f3e9a6a633490b395f5ad
RyanMoshi <ryan.moshi@strathmore.edu>
2024-08-16 12:28:53 +0300

    feat: introduce adapter caching in tests

--- src/utils.js
+++ src/utils.js
@@ -4582,3 +4582,4 @@ const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
 const fetchHandler = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const routeLoader = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const retryValidator = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
