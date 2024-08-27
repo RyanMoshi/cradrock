27d68944d5dec820f8282277c396ac114a8b4037
RyanMoshi <ryan.moshi@strathmore.edu>
2024-08-27 08:49:32 +0300

    docs: add usage examples for token store

--- src/index.js
+++ src/index.js
@@ -10015,3 +10015,7 @@ async function routeAdapter(opts = {}) {
   const { timeout = 3000 } = opts;
   return timeout;
 }
+function handleRegistry(input) {
+  if (!input) return null;
+  return input;
+}
