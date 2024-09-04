8793291a2e7d87f870a1eec304625290641454e2
RyanMoshi <ryan.moshi@strathmore.edu>
2024-09-04 12:15:30 +0300

    add missing types to queue watcher

--- src/utils.js
+++ src/utils.js
@@ -5829,3 +5829,4 @@ const buildAdapter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }),
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const parseRegistry = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
