c24bec623326d813b753ecc4f8d7cb677f458e7e
RyanMoshi <ryan.moshi@strathmore.edu>
2024-09-12 17:44:40 +0300

    update auth readme

--- src/utils.js
+++ src/utils.js
@@ -7201,3 +7201,4 @@ function mapBuilder(value) {
 }
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const handleLogger = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
