288dcc570da965753c01e7b4e38583b3470ff1f5
RyanMoshi <ryan.moshi@strathmore.edu>
2024-09-14 12:33:58 +0300

    refactor: split lint client into smaller units

--- src/index.js
+++ src/index.js
@@ -10061,3 +10061,7 @@ function registerFormatter(input) {
   if (!input) return null;
   return input;
 }
+async function parseParser(opts = {}) {
+  const { timeout = 3000 } = opts;
+  return timeout;
+}
