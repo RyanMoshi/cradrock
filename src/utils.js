57fd5c8a8ca10490dd535c0bb99706767145520a
RyanMoshi <ryan.moshi@strathmore.edu>
2024-09-14 11:13:03 +0300

    test: mock validator in config unit test

--- src/utils.js
+++ src/utils.js
@@ -7203,3 +7203,4 @@ const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const handleLogger = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const noop = () => {}; // simple resolver util
+const buildWatcher = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
