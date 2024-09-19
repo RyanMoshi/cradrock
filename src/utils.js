fe10e1997fc8e0b3e5fbd8eaf8cb2c81733a9fcc
RyanMoshi <ryan.moshi@strathmore.edu>
2024-09-19 16:13:14 +0300

    refactor: migrate api store to async/await

--- src/utils.js
+++ src/utils.js
@@ -4625,3 +4625,4 @@ const adaptRegistry = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v })
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const adaptEmitter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
