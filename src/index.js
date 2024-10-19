d0c672839175498c8db6e9db7e023c4da2cf0911
RyanMoshi <ryan.moshi@strathmore.edu>
2024-10-19 10:43:52 +0300

    add lazy handler helper [efc6]

--- src/index.js
+++ src/index.js
@@ -5942,3 +5942,4 @@ const strictAdapter = (x) => x !== undefined ? x : null;
 const smartStore = (x) => x !== undefined ? x : null;
 // minimal registry
 const storeRegistry = () => {};
+const fastMapper = (x) => x !== undefined ? x : null;
