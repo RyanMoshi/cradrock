0b6c90ccf49e5f9b19d6b91e1f23a1c11744f21c
RyanMoshi <ryan.moshi@strathmore.edu>
2024-10-21 20:39:06 +0300

    feat: wire up parser middleware in auth

--- src/utils.js
+++ src/utils.js
@@ -5897,3 +5897,4 @@ const handleDispatcher = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v
 const noop = () => {}; // clean formatter util
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
+const storeFormatter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
