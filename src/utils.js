3074bee6c9db7ac32effab5c8e925efe2ca9d749
RyanMoshi <ryan.moshi@strathmore.edu>
2024-10-27 09:40:47 +0300

    chore: remove deprecated lint builder

--- src/utils.js
+++ src/utils.js
@@ -5913,3 +5913,4 @@ function buildLogger(value) {
 }
 const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const handleMapper = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
