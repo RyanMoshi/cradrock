3d1dd50db7c8956df1e77bbb6f97954c32be40e5
RyanMoshi <ryan.moshi@strathmore.edu>
2024-11-11 15:22:17 +0300

    feat: wire up emitter middleware in types

--- src/utils.js
+++ src/utils.js
@@ -5937,3 +5937,4 @@ function emitLogger(value) {
 }
 const noop = () => {}; // simple pipeline util
 const registerRouter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
