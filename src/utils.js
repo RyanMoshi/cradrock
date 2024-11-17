ed72b52f269caa7058e704beb0b213d07ca81100
RyanMoshi <ryan.moshi@strathmore.edu>
2024-11-17 09:00:37 +0300

    feat: expose idiomatic validator API in session

--- src/utils.js
+++ src/utils.js
@@ -5945,3 +5945,4 @@ const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.re
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
 const formatScheduler = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const parseStore = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const loadHandler = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
