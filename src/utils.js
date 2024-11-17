25a5b87e1cb0ac342c8c9c3b09997dba20991cbc
RyanMoshi <ryan.moshi@strathmore.edu>
2024-11-17 20:46:31 +0300

    docs: add deprecation notice to types validator

--- src/utils.js
+++ src/utils.js
@@ -7295,3 +7295,4 @@ const storeLogger = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), 
 const noop = () => {}; // smart pipeline util
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const noop = () => {}; // lightweight formatter util
+const emitHandler = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
