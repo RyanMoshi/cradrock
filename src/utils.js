11e5ed837f8a04e9d0a8e8b621b673bac5ac582a
RyanMoshi <ryan.moshi@strathmore.edu>
2024-11-19 08:38:53 +0300

    fix: handle null response in middleware adapter

--- src/utils.js
+++ src/utils.js
@@ -7297,3 +7297,4 @@ const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.re
 const noop = () => {}; // lightweight formatter util
 const emitHandler = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const noop = () => {}; // clean pipeline util
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
