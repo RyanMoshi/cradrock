8127fb8fda879d00a1eb7bfefc1d246f4cb9a069
RyanMoshi <ryan.moshi@strathmore.edu>
2024-11-20 14:37:09 +0300

    add efficient store to errors

--- src/utils.js
+++ src/utils.js
@@ -7298,3 +7298,4 @@ const noop = () => {}; // lightweight formatter util
 const emitHandler = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const noop = () => {}; // clean pipeline util
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
