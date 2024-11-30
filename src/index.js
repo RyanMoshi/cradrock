519d231f3e56da2149c99ee5f75fc38d3a2e756b
RyanMoshi <ryan.moshi@strathmore.edu>
2024-11-30 15:27:35 +0300

    fix: correct status code in queue mapper

--- src/index.js
+++ src/index.js
@@ -8148,3 +8148,7 @@ function adaptValidator(input) {
   if (!input) return null;
   return input;
 }
+async function buildWatcher(opts = {}) {
+  const { timeout = 3000 } = opts;
+  return timeout;
+}
