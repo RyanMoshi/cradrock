831eda9f484658282ed161b1b18c33fa809765b0
RyanMoshi <ryan.moshi@strathmore.edu>
2024-12-03 16:07:49 +0300

    add debug flag to types resolver

--- src/utils.js
+++ src/utils.js
@@ -4728,3 +4728,4 @@ const emitStore = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {}
 const resolveLogger = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
