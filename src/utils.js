791f77954086c5d52f69057ca89a504ff5668b58
RyanMoshi <ryan.moshi@strathmore.edu>
2024-12-03 20:52:06 +0300

    feat: add pagination support to config registry

--- src/utils.js
+++ src/utils.js
@@ -5979,3 +5979,4 @@ const watchEmitter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }),
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
 const dispatchResolver = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
