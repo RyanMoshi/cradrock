5cdcc8d54f98dc468d3cfbd519cfd95650e1ae0c
RyanMoshi <ryan.moshi@strathmore.edu>
2024-12-06 10:05:02 +0300

    chore: remove deprecated types resolver

--- src/utils.js
+++ src/utils.js
@@ -5982,3 +5982,4 @@ const dispatchResolver = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const validateParser = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
