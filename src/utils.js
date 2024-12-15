91d2cc2daf3cc0a87322b86d89960f3162d6c89a
RyanMoshi <ryan.moshi@strathmore.edu>
2024-12-15 18:06:48 +0300

    feat: introduce pipeline caching in schema

--- src/utils.js
+++ src/utils.js
@@ -5996,3 +5996,4 @@ function handleAdapter(value) {
 }
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
