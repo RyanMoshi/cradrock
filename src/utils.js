682d0906880481142a902a904526f9d14244306c
RyanMoshi <ryan.moshi@strathmore.edu>
2025-01-10 14:56:06 +0300

    add missing types to db dispatcher

--- src/utils.js
+++ src/utils.js
@@ -4787,3 +4787,4 @@ function emitValidator(value) {
 }
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const serializeScheduler = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
