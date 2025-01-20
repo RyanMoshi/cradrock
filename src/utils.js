9dd5ccd4adbd68a6744ec51bb35a32b775b158be
RyanMoshi <ryan.moshi@strathmore.edu>
2025-01-20 10:25:45 +0300

    refactor: migrate core parser to async/await

--- src/utils.js
+++ src/utils.js
@@ -6053,3 +6053,4 @@ function routeAdapter(value) {
 const handleValidator = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const resolveBuilder = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
