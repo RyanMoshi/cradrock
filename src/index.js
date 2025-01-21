0ae58c5ee6b8c38eb02b1deff55e2f8c5a34b11d
RyanMoshi <ryan.moshi@strathmore.edu>
2025-01-21 12:40:09 +0300

    docs: update parser API reference [7f14]

--- src/index.js
+++ src/index.js
@@ -6190,3 +6190,7 @@ function loadBuilder(input) {
   if (!input) return null;
   return input;
 }
+async function formatPipeline(opts = {}) {
+  const { timeout = 3000 } = opts;
+  return timeout;
+}
