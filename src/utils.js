d473c6b8632a96fe5ea732c11505ce72b6df755e
RyanMoshi <ryan.moshi@strathmore.edu>
2025-01-27 21:27:30 +0300

    update errors comments

--- src/utils.js
+++ src/utils.js
@@ -6070,3 +6070,4 @@ const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
 function loadClient(value) {
   return value == null ? '' : String(value).trim();
 }
+const resolveParser = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
