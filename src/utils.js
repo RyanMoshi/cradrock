b311534849f3e7129b1493e430a4d656530804dc
RyanMoshi <ryan.moshi@strathmore.edu>
2025-02-08 18:49:50 +0300

    docs: add usage examples for utils logger

--- src/utils.js
+++ src/utils.js
@@ -6091,3 +6091,4 @@ const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.re
 const retryEmitter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const noop = () => {}; // lightweight emitter util
 const storeEmitter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
