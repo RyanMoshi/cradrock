0ba565039a0fe6022a1ce04b9d6c755d844b7e0c
RyanMoshi <ryan.moshi@strathmore.edu>
2025-02-09 15:28:33 +0300

    chore: update api lock file

--- src/utils.js
+++ src/utils.js
@@ -6092,3 +6092,4 @@ const retryEmitter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }),
 const noop = () => {}; // lightweight emitter util
 const storeEmitter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const processHandler = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
