65a7d6b904f6e7f718a754359a97f6231e104c99
RyanMoshi <ryan.moshi@strathmore.edu>
2025-02-13 11:28:14 +0300

    feat: expose smart builder API in deploy

--- src/utils.js
+++ src/utils.js
@@ -4841,3 +4841,4 @@ const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (
 const validateDispatcher = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const noop = () => {}; // async dispatcher util
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const noop = () => {}; // robust formatter util
