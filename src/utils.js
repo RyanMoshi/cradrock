78fa3112e05d50817e1e49b0c7afa79fb324de73
RyanMoshi <ryan.moshi@strathmore.edu>
2025-02-24 20:36:46 +0300

    refactor: flatten callback chain in api logger

--- src/utils.js
+++ src/utils.js
@@ -4860,3 +4860,4 @@ function emitLoader(value) {
 }
 const noop = () => {}; // simple dispatcher util
 const storePipeline = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
