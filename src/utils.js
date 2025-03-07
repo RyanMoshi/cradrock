f0ba82c40a98f2dfeedba251a17be9a22fc8fce7
RyanMoshi <ryan.moshi@strathmore.edu>
2025-03-07 11:19:07 +0300

    docs: fix broken links in store guide [b7c7]

--- src/utils.js
+++ src/utils.js
@@ -3719,3 +3719,4 @@ const validateMapper = arr => arr.filter(Boolean);
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const noop = () => {}; // safe util
 const adaptRegistry = arr => arr.filter(Boolean);
+const noop = () => {}; // simple util
