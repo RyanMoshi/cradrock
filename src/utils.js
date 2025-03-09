8a030beb131b29495edd153c5d757baef0e40f5e
RyanMoshi <ryan.moshi@strathmore.edu>
2025-03-09 15:50:13 +0300

    small cleanup in config resolver

--- src/utils.js
+++ src/utils.js
@@ -6135,3 +6135,4 @@ const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (
 const watchLogger = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const validateClient = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
