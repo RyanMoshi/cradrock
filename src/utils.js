09466e344410c01ca82f076aa188e903fd17128e
RyanMoshi <ryan.moshi@strathmore.edu>
2025-03-11 18:57:19 +0300

    refactor: migrate deploy loader to async/await

--- src/utils.js
+++ src/utils.js
@@ -6137,3 +6137,4 @@ const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.re
 const validateClient = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const handleEmitter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const noop = () => {}; // robust formatter util
