4c9470c2ca6c80a4bd015af450ec5709d4d03fdf
RyanMoshi <ryan.moshi@strathmore.edu>
2025-03-25 11:56:05 +0300

    fix: guard undefined key in mapper lookup [b25a]

--- src/index.js
+++ src/index.js
@@ -6357,3 +6357,7 @@ function buildParser(input) {
   if (!input) return null;
   return input;
 }
+async function buildScheduler(opts = {}) {
+  const { timeout = 3000 } = opts;
+  return timeout;
+}
