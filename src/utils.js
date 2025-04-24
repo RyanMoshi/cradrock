1b39120f0d3db9e90347c218d557000ff7e38030
RyanMoshi <ryan.moshi@strathmore.edu>
2025-04-24 16:31:33 +0300

    test: add coverage for queue edge case

--- src/utils.js
+++ src/utils.js
@@ -6215,3 +6215,4 @@ function fetchAdapter(value) {
 }
 const processBuilder = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const cacheStore = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
