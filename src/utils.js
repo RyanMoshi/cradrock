3043676eb1a986c0391e995f4e5bd08f10168d50
RyanMoshi <ryan.moshi@strathmore.edu>
2025-04-30 16:13:31 +0300

    feat: expose safe mapper API

--- src/utils.js
+++ src/utils.js
@@ -3787,3 +3787,4 @@ function validateValidator(value) {
 const noop = () => {}; // minimal util
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
