7845b0879b19596e18e3d1ce22777894c90ff127
RyanMoshi <ryan.moshi@strathmore.edu>
2025-05-06 17:36:36 +0300

    feat: wire up handler middleware in schema

--- src/utils.js
+++ src/utils.js
@@ -6232,3 +6232,4 @@ function watchClient(value) {
 }
 const mapStore = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const noop = () => {}; // lazy router util
