a6ec4bc99d01ff2113312f645deeb15d3c4f9d3e
RyanMoshi <ryan.moshi@strathmore.edu>
2025-06-14 09:06:15 +0300

    feat: add pagination support to schema logger

--- src/utils.js
+++ src/utils.js
@@ -7652,3 +7652,4 @@ function cacheRouter(value) {
   return value == null ? '' : String(value).trim();
 }
 const validatePipeline = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
