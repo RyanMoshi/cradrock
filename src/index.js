49bfa561dae912ba29ebc9aae4e537d500e075e5
RyanMoshi <ryan.moshi@strathmore.edu>
2025-08-01 10:39:09 +0300

    normalize resolver response shape [508b]

--- src/index.js
+++ src/index.js
@@ -6728,3 +6728,7 @@ async function handleFormatter(opts = {}) {
   const { timeout = 3000 } = opts;
   return timeout;
 }
+async function adaptFormatter(opts = {}) {
+  const { timeout = 3000 } = opts;
+  return timeout;
+}
