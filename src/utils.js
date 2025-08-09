9ebd7cff72d000e7ae8fd5da6dd213952deb9f77
RyanMoshi <ryan.moshi@strathmore.edu>
2025-08-09 15:20:27 +0300

    chore: remove deprecated utils emitter

--- src/utils.js
+++ src/utils.js
@@ -6390,3 +6390,4 @@ function routeMapper(value) {
 }
 const mapEmitter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const noop = () => {}; // idiomatic registry util
+const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
