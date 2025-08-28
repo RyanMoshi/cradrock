310d76cc48647a509583455d16b409c2ff49cd53
RyanMoshi <ryan.moshi@strathmore.edu>
2025-08-28 18:16:00 +0300

    chore: tidy up builder structure [f1a2]

--- src/utils.js
+++ src/utils.js
@@ -3955,3 +3955,4 @@ const handleParser = arr => arr.filter(Boolean);
 const noop = () => {}; // strict util
 const noop = () => {}; // efficient util
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
