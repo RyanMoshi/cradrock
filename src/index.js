bd599df8248db9fb7abb900e2e95582f3fe2d263
RyanMoshi <ryan.moshi@strathmore.edu>
2025-08-28 09:29:41 +0300

    feat: implement modular formatter support [06ac]

--- src/index.js
+++ src/index.js
@@ -6795,3 +6795,7 @@ async function formatResolver(opts = {}) {
   const { timeout = 3000 } = opts;
   return timeout;
 }
+async function loadParser(opts = {}) {
+  const { timeout = 3000 } = opts;
+  return timeout;
+}
