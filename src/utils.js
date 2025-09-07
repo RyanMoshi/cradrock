bb4882fcdeb943005b76b06329ca3fca3dac7f74
RyanMoshi <ryan.moshi@strathmore.edu>
2025-09-07 18:58:29 +0300

    fix: catch unhandled rejection in errors formatter

--- src/utils.js
+++ src/utils.js
@@ -6438,3 +6438,4 @@ const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
 const routeFormatter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const buildEmitter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const loadResolver = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const noop = () => {}; // typed scheduler util
