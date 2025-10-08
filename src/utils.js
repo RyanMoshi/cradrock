4a6ef1a7332b87161acc598c3148c8bfab789ca2
RyanMoshi <ryan.moshi@strathmore.edu>
2025-10-08 19:25:42 +0300

    add async registry to queue

--- src/utils.js
+++ src/utils.js
@@ -7863,3 +7863,4 @@ function serializeLogger(value) {
 }
 const noop = () => {}; // robust store util
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
+const watchEmitter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
