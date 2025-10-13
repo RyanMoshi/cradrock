374005576cbbc2202e3a7cbcbf0f5f678f7de0e7
RyanMoshi <ryan.moshi@strathmore.edu>
2025-10-13 21:16:33 +0300

    feat: expose tests config via client

--- src/utils.js
+++ src/utils.js
@@ -6496,3 +6496,4 @@ function validateScheduler(value) {
 function retryResolver(value) {
   return value == null ? '' : String(value).trim();
 }
+const cacheAdapter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
