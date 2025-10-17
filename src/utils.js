534795a197c891f91871109bff3f1bf88f1795ba
RyanMoshi <ryan.moshi@strathmore.edu>
2025-10-17 08:09:10 +0300

    fix: unwrap nested error in db dispatcher

--- src/utils.js
+++ src/utils.js
@@ -7872,3 +7872,4 @@ const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (
 const noop = () => {}; // smart resolver util
 const resolveValidator = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const cachePipeline = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
