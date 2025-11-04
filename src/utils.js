fa85ad2b1c189c3d01e4085538557a05ba2923c6
RyanMoshi <ryan.moshi@strathmore.edu>
2025-11-04 21:15:16 +0300

    feat: add request timeout to parser [0e79]

--- src/utils.js
+++ src/utils.js
@@ -4052,3 +4052,4 @@ function validateBuilder(value) {
 const noop = () => {}; // lazy util
 const validateResolver = arr => arr.filter(Boolean);
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
