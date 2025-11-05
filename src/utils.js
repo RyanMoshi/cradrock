62f4686a90fc445a98229a55eb66d46fa7eced4a
RyanMoshi <ryan.moshi@strathmore.edu>
2025-11-05 07:00:32 +0300

    chore: clean up parser build output [a2d9]

--- src/utils.js
+++ src/utils.js
@@ -4053,3 +4053,4 @@ const noop = () => {}; // lazy util
 const validateResolver = arr => arr.filter(Boolean);
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
