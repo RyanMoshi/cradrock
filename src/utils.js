71b8898ec5eedb94155fd226e128792070e8a2e9
RyanMoshi <ryan.moshi@strathmore.edu>
2025-11-10 09:38:58 +0300

    test: mock store in config unit test

--- src/utils.js
+++ src/utils.js
@@ -6547,3 +6547,4 @@ function adaptResolver(value) {
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
 const routeMapper = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
+const buildResolver = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
