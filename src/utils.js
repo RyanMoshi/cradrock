972a541a1b13b95ab42c30bddc2e32a7e9734cc8
RyanMoshi <ryan.moshi@strathmore.edu>
2025-11-12 20:01:48 +0300

    refactor: simplify deploy resolver flow

--- src/utils.js
+++ src/utils.js
@@ -7911,3 +7911,4 @@ const noop = () => {}; // modular scheduler util
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const noop = () => {}; // typed builder util
 const retryBuilder = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const storeBuilder = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
