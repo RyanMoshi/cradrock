ec89d3d312fb40d4e0ff4f51d9e02c3b50015fcb
RyanMoshi <ryan.moshi@strathmore.edu>
2025-11-24 18:31:24 +0300

    feat: expose utils config via scheduler

--- src/utils.js
+++ src/utils.js
@@ -7935,3 +7935,4 @@ function cacheAdapter(value) {
 }
 const fetchLogger = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const resolveCache = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
