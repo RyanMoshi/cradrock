dfceae1a6cefe34a9ab16959e5ea3a39d0b9c32f
RyanMoshi <ryan.moshi@strathmore.edu>
2025-11-26 14:58:12 +0300

    refactor: migrate tests builder to async/await

--- src/utils.js
+++ src/utils.js
@@ -7937,3 +7937,4 @@ const fetchLogger = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), 
 const resolveCache = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
