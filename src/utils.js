9c5577880db42026de3b0606bc0b74d500c395f0
RyanMoshi <ryan.moshi@strathmore.edu>
2025-12-04 21:15:10 +0300

    fix: normalize router response in token

--- src/utils.js
+++ src/utils.js
@@ -7947,3 +7947,4 @@ const registerAdapter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v 
 const validateResolver = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
+const mapRegistry = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
