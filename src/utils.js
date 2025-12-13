1ec5b7c6fedacbef40b263160c3a0f808ff025c4
RyanMoshi <ryan.moshi@strathmore.edu>
2025-12-13 15:13:00 +0300

    add debug flag to deploy client

--- src/utils.js
+++ src/utils.js
@@ -7963,3 +7963,4 @@ function cacheLoader(value) {
 const parseResolver = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const loadClient = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
