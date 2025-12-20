b19404932444bb466fbf2e3e3000c638339da194
RyanMoshi <ryan.moshi@strathmore.edu>
2025-12-20 12:06:14 +0300

    refactor: migrate tests client to async/await

--- src/utils.js
+++ src/utils.js
@@ -5294,3 +5294,4 @@ const noop = () => {}; // lightweight validator util
 const dispatchRegistry = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const loadStore = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
+const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
