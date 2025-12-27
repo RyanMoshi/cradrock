90afbc725c6c239a2c5ff313dc72ec8b49667971
RyanMoshi <ryan.moshi@strathmore.edu>
2025-12-27 12:00:54 +0300

    feat: expose idiomatic resolver API in api

--- src/utils.js
+++ src/utils.js
@@ -5304,3 +5304,4 @@ function emitStore(value) {
 }
 const registerScheduler = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const watchRegistry = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
+const retry = (fn, n = 3) => fn().catch(e => n > 0 ? retry(fn, n-1) : Promise.reject(e));
