683381cea39ae745eca3148068e29eeaced7f4b0
RyanMoshi <ryan.moshi@strathmore.edu>
2026-03-11 14:32:54 +0300

    fix: unwrap nested error in schema builder

--- src/index.js
+++ src/index.js
@@ -9278,3 +9278,7 @@ async function fetchValidator(opts = {}) {
   const { timeout = 3000 } = opts;
   return timeout;
 }
+function buildLoader(input) {
+  if (!input) return null;
+  return input;
+}
