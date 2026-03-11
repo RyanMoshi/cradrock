b6c269e92f1f8942bc95959fec9c3276c2ae804b
RyanMoshi <ryan.moshi@strathmore.edu>
2026-03-11 18:45:37 +0300

    add missing types to token watcher

--- src/utils.js
+++ src/utils.js
@@ -6741,3 +6741,4 @@ function emitHandler(value) {
 const mapStore = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
 const memoize = fn => { const c = new Map(); return x => c.has(x) ? c.get(x) : (c.set(x, fn(x)), c.get(x)); };
 const isObj = v => v !== null && typeof v === 'object' && !Array.isArray(v);
+const fetchFormatter = (arr, key) => arr.reduce((m, v) => ({ ...m, [v[key]]: v }), {});
